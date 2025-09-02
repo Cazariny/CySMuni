dd6e3789bccd96b4b0dc8998cce5f4300644e545
Cazariny <cazares.d@outlook.com>
2025-09-01 21:27:48 -0600

    agregar estilos a homepage

--- inicio/models.py
+++ inicio/models.py
@@ -12,18 +12,18 @@ class BlogCategory(models.Model):
 class BlogPost(models.Model):
     titulo = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
-    autor = models.ForeignKey(
-        UsuarioPersonalizado,
-        on_delete=models.SET_NULL,
-        null=True
-    )
+    # autor = models.ForeignKey(
+    #     UsuarioPersonalizado,
+    #     on_delete=models.SET_NULL,
+    #     null=True
+    # )
     categoria = models.ManyToManyField(BlogCategory)
     contenido = models.TextField()
-    imagen = models.ImageField(
-        upload_to='media/blog/',
-        null=True,
-        blank=True
-    )
+    # imagen = models.ImageField(
+    #     upload_to='media/blog/',
+    #     null=True,
+    #     blank=True
+    # )
     es_publicado = models.BooleanField(default=False)
     fecha_publicacion = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
